cc80148fc431a5abd4ecb62d8b5d845e9d5a88bb
Tester <shivamsahu0448@gmail.com>
2026-07-24 18:13:40 +0530

    Add GitHub Actions workflow

--- java/utils/DriverFactory.java
+++ java/utils/DriverFactory.java
@@ -1,8 +1,39 @@
+//package utils;
+//
+//import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//public class DriverFactory {
+//
+//    private static WebDriver driver;
+//
+//    public static WebDriver getDriver() {
+//
+//        if (driver == null) {
+//            WebDriverManager.chromedriver().setup();
+//            driver = new ChromeDriver();
+//            driver.manage().window().maximize();
+//        }
+//
+//        return driver;
+//    }
+//
+//    public static void quitDriver() {
+//        if (driver != null) {
+//            driver.quit();
+//            driver = null;
+//        }
+//    }
+//}
+
+
 package utils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
@@ -12,8 +43,26 @@ public class DriverFactory {
 
         if (driver == null) {
             WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-            driver.manage().window().maximize();
+
+            ChromeOptions options = new ChromeOptions();
+
+            // Detect if running inside GitHub Actions or CI environment
+            boolean isCI = System.getenv("GITHUB_ACTIONS") != null || Boolean.parseBoolean(System.getProperty("headless"));
+
+            if (isCI) {
+                // Headless options required for GitHub Actions Linux runner
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--disable-gpu");
+                options.addArguments("--window-size=1920,1080");
+            }
+
+            driver = new ChromeDriver(options);
+
+            if (!isCI) {
+                driver.manage().window().maximize();
+            }
         }
 
         return driver;
